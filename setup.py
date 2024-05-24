68b1ffc838f304e19a85c5c83d3d0cb0989ae6da
rodfer0x80 <rodrigolf080@gmail.com>
2024-05-24 12:53:17 +0100

    sync

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pypeline',
     version='1.0.1',
+    python_requires='>=3.6, <4.0',
     packages=find_packages(),
     install_requires=[
         "pytest",
